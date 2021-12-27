c6d5ba2207e88872ff146c82a72cef83b9c996aa
Bibhash Singh <bibhash1996@gmail.com>
2021-12-27 20:21:53 +0530

    add proposals

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -21,10 +21,35 @@ contract Voting {
         mapping(address => Vote) votes;
     }
 
+    mapping(string => Proposal) public proposals;
     event CreatedProposalEvent(string name);
     event CreatedVoteEvent(string name);
 
     constructor() public {
         owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(
+            msg.sender == owner,
+            "Only owners or admins can add a proposal"
+        );
+        _;
+    }
+
+    //proposal name has to be unique
+    function addProposal(
+        string memory name,
+        string memory description,
+        string memory year
+    ) public onlyOwner returns (bool) {
+        Proposal memory proposal = Proposal({
+            name: name,
+            description: description,
+            year: year
+            //    votes:{}
+        });
+        proposals[name] = proposal;
+        return true;
     }
 }
